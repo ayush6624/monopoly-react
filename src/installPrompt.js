e4c8aa951a562ca321b21621d2010f4534a243c2
Ayush Goyal <ayushg1214@gmail.com>
2021-04-11 17:20:14 +0530

    feat: add notifications

--- src/installPrompt.js
+++ src/installPrompt.js
@@ -1,11 +1,12 @@
 import PWAInstallerPrompt from 'react-pwa-installer-prompt';
 import { Button } from '@geist-ui/react';
+import { pwa } from './Home';
 
 const Install = () => {
   return (
     <PWAInstallerPrompt
       render={({ onClick }) => (
-        <Button size='mini' type="Button" onClick={onClick}>
+        <Button size="mini" type="Button" onClick={onClick}>
           Install
         </Button>
       )}
